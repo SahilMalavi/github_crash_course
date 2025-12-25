22ef47696ff05e221e109f0971f18f6afc7af4e0
Sahil Malavi <sahilmalavi96@gmail.com>
2025-12-25 19:14:05 +0530

    commit 3

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { age,name, greet, isAbove18 } = require("./util.js");
 function getInfo() {
   console.log("Getting user info...");
   console.log("---------------------");
+  console.log(`Name: ${name}`);
   console.log(greet(name));
   console.log(`${name} age is above 18: ${isAbove18(age)}`);
   console.log(`Age is ${age}`);
