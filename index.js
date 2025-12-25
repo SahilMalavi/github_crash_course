42eff26ffc9dc8e974bd0d5ca2aa8fcb3e9d1b8d
Sahil Malavi <sahilmalavi96@gmail.com>
2025-12-25 19:09:58 +0530

    all functions combined into one getInfo to display

--- index.js
+++ index.js
@@ -1,18 +1,9 @@
 const { age,name, greet, isAbove18 } = require("./util.js");
 
 function getInfo() {
-  console.log(greet("Bob"));
-  console.log(`bob age is above 18: ${isAbove18(20)}`);
+  console.log(greet(name));
+  console.log(`${name} age is above 18: ${isAbove18(age)}`);
   console.log(`Age is ${age}`);
 }
 
-
-function displayName() {
-  console.log(`Name is ${name}`);
-}
-
-function displayAge() {
-  console.log(`Age is ${age}`);
-}
-
-displayName();
+getInfo();
